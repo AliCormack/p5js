481fa1146438ce9cc2679a4aad97f08a6fce0115
AliCormack <abgc500@york.ac.uk>
2017-03-19 20:14:35 +0000

    Basic fractal tree with branch angle slider

--- fractal/tree/processing.js
+++ fractal/tree/processing.js
@@ -1,9 +1,31 @@
+var slider;
+var angle = 0;
+
 function setup()
 {
-
+  createCanvas(600, 600);
+  background(70);
+  slider = createSlider(-2*PI, 2*PI, PI/4, 0.01);
 }
 
 function draw()
 {
-  
+  stroke(255);
+  translate(width/2, height);
+  branch(100, 1);
+  angle = slider.value();
+}
+
+function branch(len, mult)
+{
+  line(0, 0, 0, -len);
+  translate(0, -len);
+  rotate(angle);
+
+  var newLength = len*0.8;
+  if(newLength > 5)
+  {
+      branch(newLength, 5);
+  }
+
 }
